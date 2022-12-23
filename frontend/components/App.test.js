47688325f579003ec6fb06244ccfe9b317fc275a
Xavier Buckner <xavier@Xaviers-MacBook-Pro.local>
2022-12-22 21:03:31 -0600

    Added tests

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,29 +1,16 @@
-// Write your tests here
-import React from "react"
-import * as rtl from "react-testing-library"
-import 'jest-dom/extend-expect'
-import AppFunctional from "./AppFunctional"
-import AppClass from "./AppClass"
+import React from 'react';
+import { shallow } from 'enzyme';
+import AppFunctional from './AppFunctional';
 
+describe('AppFunctional', () => {
+  it('should show the active square on mount', () => {
+    // Arrange
+    const wrapper = shallow(<AppFunctional />);
 
-Copy code
-it("Only accepts valid emails", () => {
-  const { getByLabelText, getByText } = rtl.render(<AppClass />)
-  const emailInput = getByLabelText("email")
-  const submitButton = getByText("Submit")
+    // Act
+    wrapper.instance().componentDidMount();
 
-  // Try to submit the form with an invalid email
-  rtl.fireEvent.change(emailInput, { target: { value: "invalid" } })
-  rtl.fireEvent.click(submitButton)
-
-  // Check that the error message is displayed
-  const errorMessage = getByText("Please enter a valid email")
-  expect(errorMessage).toBeInTheDocument()
-
-  // Try to submit the form with a valid email
-  rtl.fireEvent.change(emailInput, { target: { value: "valid@email.com" } })
-  rtl.fireEvent.click(submitButton)
-
-  // Check that the error message is not displayed
-  expect(errorMessage).not.toBeInTheDocument()
-})
+    // Assert
+    expect(wrapper.find('.square.active').length).toEqual(1);
+  });
+});
